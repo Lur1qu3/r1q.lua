f10cb55f2549a22ba4a275b0daba82e402121190
Lur1qu3 <mcmclhmm1@gmail.com>
2024-09-10 22:57:15 -0300

    Update r1q.lua

--- r1q.lua
+++ r1q.lua
@@ -1,4 +1,3 @@
---[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]--
 
 
 
@@ -1491,7 +1490,51 @@ Stairs.walk.setOff()
 
 
 
+UI.Button("Amiguinhos", function(newText)
+  UI.MultilineEditorWindow(storage.FriendText or "", {title="Amigos", description="Adicionados", width=250, height=200}, function(text)
+      storage.FriendText = text
+      reload()
+  end)
+end)
 
+isAmigo = function(name)
+  if type(name) ~= 'string' then
+      name = name:getName()
+  end
+  local tabela = storage.FriendText:split('\n')
+  tabela = #tabela > 0 and tabela or false
+  if not tabela or #tabela == 0 then
+    return false
+end
+      return table.find(tabela, name:trim(), true)
+  end
+
+
+
+local enemyMacro = macro(100, 'Enemy', function()
+  local possibleTarget = false
+  for _, creature in ipairs(getSpectators(posz())) do
+      local specHP = creature:getHealthPercent()
+      if creature:isPlayer() and specHP and specHP > 0 then
+          if not isAmigo(creature) and creature:getEmblem() ~= 1 then
+              if creature:canShoot(9) then
+                  if not possibleTarget or possibleTargetHP > specHP or (possibleTargetHP == specHP and possibleTarget:getId() < creature:getId()) then
+                      possibleTarget = creature
+                      possibleTargetHP = possibleTarget:getHealthPercent()
+                  end
+              end
+          end
+      end
+  end
+  if possibleTarget and g_game.getAttackingCreature() ~= possibleTarget then
+      g_game.attack(possibleTarget)
+  end
+end)
+
+
+posEnemy = addIcon("Enemy", {item = 21979, text = "Enemy"}, enemyMacro)
+posEnemy:breakAnchors()
+posEnemy:move(200, 450)
 
 
 
